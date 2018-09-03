a56a6cf91b12a8661a1171e46907db8b277a9f3d
RagavendranMRN <36659683+RagavendranMRN@users.noreply.github.com>
2018-09-03 19:44:28 +0530

    Update and rename googlescrap.py to Google_Results.py

--- Selenium/Google_search_result Scrapping/Google_Results.py
+++ Selenium/Google_search_result Scrapping/Google_Results.py
@@ -1,8 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-totalrating =0
-count = 1;
+
 def init_driver():
     driver = webdriver.Chrome()
     return driver
@@ -10,21 +9,13 @@ def init_driver():
 def scrap(driver,query):
     driver.get("http://www.google.com/search?q="+query)
     links = driver.find_elements_by_xpath("//h3[@class='r']/a[@href]")
-    try:
-        rating = driver.find_elements_by_class_name('slp f')
-        for rates in rating:
-            print(rates.text)
-
-    except:
-        pass
-    
     contents = driver.find_elements_by_class_name('st')
     results = []
     for link,content in zip(links,contents):
         heading = link.text
         url = link.get_attribute('href')    
-        print("\n{\n\"title\":"+heading+"\n\"url\":"+url+"\n\"content\":"+content.text+"\n}")
+        print("Title:"+heading+"\n\"Url:"+url+"\n\Content:"+content.text+"\n")
        
 driver = init_driver()
-scrap(driver,"HDFC reviews and ratings")
+scrap(driver,"Selenium")
 driver.quit()
